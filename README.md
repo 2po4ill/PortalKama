27c24c6bfe5abfb8b455e65355906c695040c7a1
2po4ill <ydevlicharov@bk.ru>
2024-02-10 14:48:08 +0300

    first logout command

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PortalKama
+Запустить отрисовку страницы: npm start
+Запустить искусственный бэкэнд: npm run start:dev:server
--- src/features/profile/ui/UserDescription/UserDescription.tsx
+++ src/features/profile/ui/UserDescription/UserDescription.tsx
@@ -2,15 +2,25 @@ import {ICollapsible, Collapsible} from "shared/ui/Collapsible/Collapsible";
 import cls from './UserDescription.module.scss';
 import React, {FC} from "react";
 import {Button} from "shared/ui/Button/Button";
+import {userActions} from "entities/User";
+import {useAppDispatch} from "shared/lib/hooks/useAppDispatch";
+import {LOCAL_STORAGE_USER_KEY} from "shared/const/localstorage";
 
 export const UserDescription: FC<ICollapsible> = (props) => {
     const { className, ...other } = props;
+    const dispatch = useAppDispatch();
+
+
+    const logout = () => {
+        localStorage.setItem(LOCAL_STORAGE_USER_KEY, null);
+        dispatch(userActions.setAuthData(null));
+    }
 
     return (
         <div className={cls.UserDescription}>
             <Collapsible isClicked={props.isClicked} title={"description"} openedWindow={props.openedWindow} windowName={"Личные данные"} onClose={props.onClose}>
                 <label> Здесь что-то будет </label>
-                <Button> Выйти из аккаунта </Button>
+                <Button onClick={logout}> Выйти из аккаунта </Button>
             </Collapsible>
         </div>
 
